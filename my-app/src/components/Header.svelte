79f2a1066563451de1e58731247eb8ebd85fd557
EmreT58 <emre.taskopru@hva.nl>
2023-11-17 16:08:51 +0100

    werkgroep 17-11 barchart weergeven en proberen data te weergeven

--- my-app/src/components/Header.svelte
+++ my-app/src/components/Header.svelte
@@ -1,26 +1,80 @@
 <script>
-    import { onMount } from "svelte";
-    import * as d3 from 'd3';
-    import dataset from "../lib/data.json";
+  import { onMount } from "svelte";
+  import * as d3 from 'd3';
+  import dataset from "../lib/data.json";
 
-    
-    onMount(async () => { //Javascript komt hier in
-        console.log(dataset);
+  // Fake data for demonstration
+  // const dataset = [
+  //   { label: "Category A", value: 30 },
+  //   { label: "Category B", value: 45 },
+  //   { label: "Category C", value: 20 },
+  //   { label: "Category D", value: 60 },
+  //   { label: "Category E", value: 10 }
+  // ];
 
+  onMount(() => {
+    // Select the chart container
+    const chartContainer = d3.select("#chart-container");
 
-        // .on('mouseover touchstart', (e,d) => { == eventlister
-    });
+    // Set the width and height of the chart
+    const margin = { top: 20, right: 30, bottom: 30, left: 40 };
+    const width = 400 - margin.left - margin.right;
+    const height = 300 - margin.top - margin.bottom;
+
+    // Create an SVG element
+    const svg = chartContainer.append("svg")
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g') // Append a group element to hold the chart
+      .attr('transform', `translate(${margin.left},${margin.top})`);
+
+    // Create scales for x and y axes
+    const xScale = d3.scaleBand()
+      .domain(dataset.map((d) => d.label))
+      .range([0, width])
+      .padding(0.1);
+
+    const yScale = d3.scaleLinear()
+      .domain([0, d3.max(dataset, (d) => d.value)])
+      .range([height, 0]);
+
+    // Create bars
+    svg.selectAll("rect")
+      .data(dataset)
+      .enter()
+      .append("rect")
+      .attr("x", (d) => xScale(d.label))
+      .attr("y", (d) => yScale(d.value))
+      .attr("width", xScale.bandwidth())
+      .attr("height", (d) => height - yScale(d.value))
+      .attr("fill", "steelblue");
+
+    // Create x-axis
+    svg.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(xScale));
+
+    // Create y-axis
+    svg.append("g")
+      .call(d3.axisLeft(yScale));
+  });
 </script>
-<!-- HTML komt hier -->
-<!-- deze component gebruiken om een header te maken-->
-<section id="header">
-    <h1>header</h1>
-</section>
 
-<!-- CSS komt hiern -->
-<style> 
+
+  
+  <!-- HTML -->
+  <section>
+    <div id="chart-container"></div>
+  </section>
+  
+  <!-- CSS -->
+  <style>
     section {
-        height: 95vh;
-        background-color: lightblue;
+      height: 95vh;
+      background-color: lightblue;
+      display: flex;
+      justify-content: center;
+      align-items: center;
     }
-</style>
+  </style>
+  
--- my-app/src/components/Main.svelte
+++ my-app/src/components/Main.svelte
@@ -2,10 +2,42 @@
     import { onMount } from "svelte";
     import * as d3 from 'd3';
     
+            // Data
+let myData = [40, 10, 20, 60, 30];
     onMount(async () => { //Javascript komt hier in
         console.log('Dit is de main test');
 
-        // .on('mouseover touchstart', (e,d) => { == eventlister
+
+
+// Chart dimensions
+const width = 400;
+const height = 80;
+
+// Create an SVG element
+const svg = d3.select('#chart')
+  .attr('width', width)
+  .attr('height', height);
+
+// Create bars
+svg.selectAll('rect')
+  .data(myData)
+  .enter()
+  .append('rect')
+  .attr('x', (d, i) => i * 30)
+  .attr('y', (d) => height - d) // Invert the bars to have the taller bars at the bottom
+  .attr('width', 20)
+  .attr('height', (d) => d);
+
+// Optional: Add labels to the bars
+svg.selectAll('text')
+  .data(myData)
+  .enter()
+  .append('text')
+  .attr('x', (d, i) => i * 30 + 10)
+  .attr('y', (d) => height - d - 5) // Adjust text position for readability
+  .attr('text-anchor', 'middle') // Center the text
+  .text((d) => d);
+
 
     });
 </script>
